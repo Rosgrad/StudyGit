aeac2656f989909b9f6883e9b1758ee65ad8ac40
unknown <amir07021@mail.ru>
2023-01-27 11:14:45 +0300

    Add console

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Warrior
+{
+    public class Warrior
+    {
+        public double hp = 100;
+        public double damage;
+        public virtual void Damage()
+        {
+            double ResHp = hp - damage;
+            hp = ResHp;
+            Console.WriteLine(ResHp + "\n");
+        }
+    }
+
+    class EasyWarrior : Warrior
+    {
+        public override void Damage()
+        {
+            double ResHp = hp - damage;
+            hp = ResHp;
+            Console.WriteLine(ResHp + "\n");
+        }
+    }
+
+    class BasicWarrior : Warrior
+    {
+        public override void Damage()
+        {
+            double ResHp = hp - (damage / 1.5);
+            hp = ResHp;
+            Console.WriteLine(ResHp + "\n");
+        }
+    }
+
+    class HardWarrior : Warrior
+    {
+        public override void Damage()
+        {
+            double ResHp = hp - (damage / 2);
+            hp = ResHp;
+            Console.WriteLine(ResHp + "\n");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EasyWarrior EW = new EasyWarrior();
+            BasicWarrior BW = new BasicWarrior();
+            HardWarrior HW = new HardWarrior();
+        re:
+            int vblbor = Convert.ToInt32(Console.ReadLine());
+            if (vblbor == 1)
+            {
+                int Damage = Convert.ToInt32(Console.ReadLine());
+                EW.damage = Damage;
+                EW.Damage();
+               
+            }
+            if (vblbor == 2)
+            {
+                int Damage = Convert.ToInt32(Console.ReadLine());
+                BW.damage = Damage;
+                BW.Damage();
+            }
+            if (vblbor == 3)
+            {
+                int Damage = Convert.ToInt32(Console.ReadLine());
+                HW.damage = Damage;
+                HW.Damage();
+            }
+            goto re;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,61 @@
+﻿
+
+public class Warrior
+{
+    public double hp = 100;
+    public double damage;
+    public virtual void Damage()
+    {
+
+
+        Console.WriteLine( "Поучает  урона");
+    }
+
+}
+
+public class Dodger : Warrior
+{
+
+    public  override void Damage()
+    {
+
+        Console.WriteLine("Поучает больше  урона");
+    }
+
+}
+
+public class Tank : Warrior
+{
+
+    public override void Damage()
+    {
+        Console.WriteLine("Поучает меньше  урона");
+    }
+
+}
+
+
+public class Magician : Warrior
+{
+
+
+
+}
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число");
+        Dodger EW = new Dodger();
+        Tank T = new Tank();
+ 
+        int Damagge = Convert.ToInt32(Console.ReadLine());
+        if (Damagge == 1) 
+        EW.Damage();
+        else if (Damagge == 2)
+            T.Damage();
+
+        Console.ReadKey();
+    }
+}
